2fb8f89d4453da400b5044291b61fa6e47278c70
PYXHD <marc-charvot@hotmail.fr>
2026-03-06 16:51:37 +0100

    modify getMonthData and add getMonthData.test

---
+++ src/domain/stats/getMonthData.test.ts
@@ -0,0 +1,44 @@
+import { describe, test, expect } from "vitest";
+import getMonthData from "./getMonthData";
+import type { Entry } from "@/types/Entry";
+
+describe("getMonthData()", () => {
+  const entries: Entry[] = [
+    { date: "2025-11-05", value: 2 },
+    { date: "2025-11-06", value: 3 },
+    { date: "2025-12-01", value: 3 },
+    { date: "2025-12-02", value: 3 },
+    { date: "2025-12-03", value: 2 },
+    { date: "2026-01-01", value: 4 },
+    { date: "2026-11-15", value: 2 },
+    { date: "2026-11-26", value: 3 },
+  ];
+
+  describe("normal cases", () => {
+    test("returns correct month's entries", () => {
+      const expected: Entry[] = [
+        { date: "2025-12-01", value: 3 },
+        { date: "2025-12-02", value: 3 },
+        { date: "2025-12-03", value: 2 },
+      ];
+      const result = getMonthData(entries, "12", "2025");
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe("edge cases", () => {
+    test("filters by month AND year (ignores same month in other years)", () => {
+      const expected: Entry[] = [
+        { date: "2025-11-05", value: 2 },
+        { date: "2025-11-06", value: 3 },
+      ];
+      const result = getMonthData(entries, "11", "2025");
+      expect(result).toEqual(expected);
+    });
+
+    test("returns empty array if no data", () => {
+      const result = getMonthData(entries, "09", "2025");
+      expect(result).toEqual([]);
+    });
+  });
+});
---
+++ src/domain/stats/getMonthData.ts
@@ -0,0 +1,15 @@
+import type { Entry } from "@/types/Entry";
+import type { Year, Month } from "@/types/dateTypes";
+
+function getMonthData(entries: Entry[], month: Month, year: Year) {
+  const monthEntries = entries.filter((entry) => {
+    const entryYear = entry.date.slice(0, 4);
+    const entryMonth = entry.date.slice(5, 7);
+
+    return entryYear === year && entryMonth === month;
+  });
+
+  return monthEntries;
+}
+
+export default getMonthData;
