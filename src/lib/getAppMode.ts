b8e61af140d63d468869fe5a9b1f3c2395a7238a
PYXHD <marc-charvot@hotmail.fr>
2026-03-10 20:30:23 +0100

    delete obsolete functions

--- src/lib/getAppMode.ts
+++
@@ -1,7 +0,0 @@
-import type { AppModeType } from "@/types/AppModeType";
-
-export function getAppMode(): AppModeType {
-  if (typeof window === "undefined") return "mock";
-
-  return localStorage.getItem("app-mode") === "real" ? "real" : "mock";
-}
