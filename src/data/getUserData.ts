8e64d3a16a78528f8f802a7501aa8f0ee0e39ae9
PYXHD <marc-charvot@hotmail.fr>
2026-03-11 08:34:57 +0100

    add getUserData V 1.0, without API

---
+++ src/data/getUserData.ts
@@ -0,0 +1,18 @@
+import { getAppMode } from "@/lib/init/getAppMode";
+import { demoUserData } from "./sources/mock/demoUserData";
+
+export async function getUserData() {
+  const mode = getAppMode();
+
+  if (mode === "mock") {
+    return demoUserData;
+  }
+
+  //   const res = await fetch("/api/user");
+
+  //   if (!res.ok) {
+  //     throw new Error("Failed to fetch user data");
+  //   }
+
+  //   return res.json;
+}
