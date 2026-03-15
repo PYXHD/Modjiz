6a83eb6dda8a9b7a925b31bf389746a15fa3836b
PYXHD <marc-charvot@hotmail.fr>
2026-03-15 14:24:51 +0100

    add NavBar V1.0

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import Header from "@/components/ui/header/Header";
+import NavBar from "@/components/ui/navBar/NavBar";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-
       <main>{children}</main>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/ui/navBar/NavBar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./NavBar.module.scss";
+
+function NavBar() {
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.container}>
+      <Link
+        href="/"
+        className={`${styles.iconButton} ${pathname === "/app" ? styles.active : ""}`}
+      >
+        <img src="/icons/menu_home.svg" alt="Home icon" />
+      </Link>
+      <Link
+        href="/app/stats"
+        className={`${styles.iconButton} ${pathname === "/app/stats" ? styles.active : ""}`}
+      >
+        <img src="/icons/menu_stats.svg" alt="Stats icon" />
+      </Link>
+      <Link
+        href="/app/settings"
+        className={`${styles.iconButton} ${pathname === "/app/settings" ? styles.active : ""}`}
+      >
+        <img src="/icons/menu_settings.svg" alt="Settings icon" />
+      </Link>
+    </div>
+  );
+}
+
+export default NavBar;
