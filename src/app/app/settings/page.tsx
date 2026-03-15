63d39ce680de023480ce11eab33e2ab513effa3a
PYXHD <marc-charvot@hotmail.fr>
2026-03-15 16:30:12 +0100

    add settings page and ThemeSwitch component

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,3 +1,47 @@
-export default function Settings() {
-  return <h1>Settings</h1>;
+import styles from "./page.module.scss";
+
+import Button from "@/components/ui/button/Button";
+import ThemeSwitch from "@/components/ui/themeSwitch/ThemeSwitch";
+
+function Settings() {
+  return (
+    <div className={styles.preferences}>
+      <h1 className={styles.titleCentered}>Préférences</h1>
+      <div className={styles.container}>
+        <div className={styles.h2Container}>
+          <h2>Apparence</h2>
+          <div className={styles.subContainer}>
+            <div className={styles.textBody}>Thème</div>
+            <ThemeSwitch />
+          </div>
+        </div>
+
+        <div className={styles.h2Container}>
+          <h2>Infos</h2>
+          <div className={styles.subContainer}>
+            <div className={styles.textBody}>Version</div>
+            <div className={styles.textBody}>0.7.0</div>
+          </div>
+          <div className={styles.subContainer}>
+            <div className={styles.textBody}>Auteur</div>
+            <div className={styles.textBody}>Marc CHARVOT</div>
+          </div>
+        </div>
+
+        <div className={styles.h2Container}>
+          <h2>Support</h2>
+          <div className={styles.btnContainer}>
+            <p className={styles.textBody}>
+              Une question ou un avis à partager ?
+            </p>
+            <a href="mailto:pyxhd@outlook.fr">
+              <Button>Donner un avis</Button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Settings;
---
+++ src/components/ui/themeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./ThemeSwitch.module.scss";
+
+export default function ThemeSwitch() {
+  const [mode, setMode] = useState<"light" | "dark">(() => {
+    if (typeof document === "undefined") return "light";
+
+    const theme = document.documentElement.getAttribute("data-theme");
+    return (theme as "light" | "dark") || "light";
+  });
+
+  const toggle = () => {
+    const newMode = mode === "light" ? "dark" : "light";
+    setMode(newMode);
+    document.documentElement.setAttribute("data-theme", newMode);
+  };
+
+  return (
+    <div className={styles.switch}>
+      <div
+        className={`${styles.slider} ${mode === "dark" ? styles.right : ""}`}
+      />
+
+      <button
+        className={mode === "light" ? styles.active : ""}
+        onClick={toggle}
+      >
+        Clair
+      </button>
+
+      <button className={mode === "dark" ? styles.active : ""} onClick={toggle}>
+        Sombre
+      </button>
+    </div>
+  );
+}
