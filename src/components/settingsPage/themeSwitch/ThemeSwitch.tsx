621908fd9aa2d40a11f3ac00e0015e15f7826a83
PYXHD <marc-charvot@hotmail.fr>
2026-04-10 11:16:33 +0200

    fix ESLint error

--- src/components/settingsPage/themeSwitch/ThemeSwitch.tsx
+++ src/components/settingsPage/themeSwitch/ThemeSwitch.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./ThemeSwitch.module.scss";
 
 export default function ThemeSwitch() {
-  const [mode, setMode] = useState<"light" | "dark">(() => {
+  const [mode, setMode] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
     const theme = document.documentElement.getAttribute("data-theme");
-    return theme === "dark" ? "dark" : "light";
-  });
+
+    if (theme === "light" || theme === "dark") {
+      setMode(theme);
+    }
+  }, []);
 
   const toggle = () => {
     const newMode = mode === "light" ? "dark" : "light";
