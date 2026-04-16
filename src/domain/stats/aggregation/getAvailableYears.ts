491824ed9c9a227d700b4ae872460cf8bda30aeb
PYXHD <marc-charvot@hotmail.fr>
2026-04-16 22:27:03 +0200

    adjust getAvailableYears and test

--- src/domain/stats/aggregation/getAvailableYears.ts
+++ src/domain/stats/aggregation/getAvailableYears.ts
@@ -4,11 +4,9 @@ export function getAvailableYears(data: Entry[]) {
   const result: number[] = [];
 
   data.forEach((entry) => {
-    const year = new Date(entry.date).getFullYear();
+    const year = Number(entry.date.slice(0, 4));
 
     const exists = result.includes(year);
-
-    if (isNaN(year)) return;
 
     if (!exists) {
       result.push(year);
