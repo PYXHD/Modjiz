b6e4e432dbd000d66f4f5647bcab3330c35f1d7b
PYXHD <marc-charvot@hotmail.fr>
2026-04-17 21:34:25 +0200

    adjust getMonthData and test

--- src/domain/stats/queries/getMonthData.ts
+++ src/domain/stats/queries/getMonthData.ts
@@ -1,12 +1,10 @@
 import type { Entry } from "@/types/Entry";
 
-export function getMonthData(entries: Entry[], month: string, year: string) {
-  const YearMonth = entries.filter((entry) => {
-    const entryYear = entry.date.slice(0, 4);
-    const entryMonth = entry.date.slice(5, 7);
+export function getMonthData(entries: Entry[], month: number, year: number) {
+  return entries.filter((entry) => {
+    const entryYear = Number(entry.date.slice(0, 4));
+    const entryMonth = Number(entry.date.slice(5, 7)) - 1;
 
     return entryYear === year && entryMonth === month;
   });
-
-  return YearMonth;
 }
