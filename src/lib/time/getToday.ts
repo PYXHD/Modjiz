48f8259d38b237f30faa3efae47d3f56630d215a
PYXHD <marc-charvot@hotmail.fr>
2026-04-25 22:13:26 +0200

    adjust getToday

--- src/lib/time/getToday.ts
+++ src/lib/time/getToday.ts
@@ -3,7 +3,7 @@ import { getAppMode } from "../init/getAppMode";
 export function getToday(): Date {
   const mode = getAppMode();
 
-  const today = mode === "mock" ? new Date("2026-03-05") : new Date();
+  const today = mode === "mock" ? new Date(2026, 2, 5) : new Date();
 
   today.setHours(0, 0, 0, 0);
 
