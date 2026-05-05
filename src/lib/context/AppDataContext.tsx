44608497b0a2eb9124835f9012274d2ff5108db0
PYXHD <161946414+PYXHD@users.noreply.github.com>
2026-05-05 21:43:42 +0200

    create useContext

---
+++ src/lib/context/AppDataContext.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import type { Entry } from "@/types/Entry";
+import type { ISODate } from "@/types/Time";
+import type { ReactNode } from "react";
+
+import { createContext, useContext } from "react";
+
+type AppData = {
+  userData: Entry[];
+  historyData: ISODate[];
+};
+
+type DataProviderProps = {
+  children: ReactNode;
+  value: AppData;
+};
+
+const AppDataContext = createContext<AppData | null>(null);
+
+export function DataProvider({ children, value }: DataProviderProps) {
+  return (
+    <AppDataContext.Provider value={value}>{children}</AppDataContext.Provider>
+  );
+}
+
+export function useAppData() {
+  const context = useContext(AppDataContext);
+
+  if (!context) {
+    throw new Error("useAppData must be used inside DataProvider");
+  }
+
+  return context;
+}
