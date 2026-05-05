2d1ac0fdb4a1e83772a2cff6054df2aaf377c052
PYXHD <161946414+PYXHD@users.noreply.github.com>
2026-05-05 22:16:38 +0200

    ajust Stats page

--- src/components/statsPage/Stats.tsx
+++ src/components/statsPage/Stats.tsx
@@ -1,37 +1,30 @@
 "use client";
 
 import styles from "./Stats.module.scss";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { getToday } from "@/lib/time/getToday";
 import { toDayKey } from "@/lib/time/toDayKey";
+import { useAppData } from "@/lib/context/AppDataContext";
 
 import Button from "../ui/button/Button";
 
 import MonthView from "./monthView/MonthView";
 import YearView from "./yearView/YearView";
 import { upsertHistoryDay } from "@/domain/mood/upsertHistoryDay";
-import { getHistoryData } from "@/data/getHistoryData";
 
 function Stats() {
   const [mode, setMode] = useState<"month" | "year">("month");
 
-  useEffect(() => {
-    async function init() {
-      const stored = await getHistoryData();
+  const { historyData } = useAppData();
 
-      const today = toDayKey(getToday());
-      const updated = upsertHistoryDay(stored, today);
-
-      sessionStorage.setItem("history-data", JSON.stringify(updated));
-    }
-
-    init();
-  }, []);
+  const today = toDayKey(getToday());
+  const updatedHistory = upsertHistoryDay(historyData, today);
 
   return (
     <div className={styles.stats}>
       <h1 className="text-center">Mon historique</h1>
+
       <div className={styles.buttonContainer}>
         <Button
           variant={mode === "month" ? "primary" : "outline"}
