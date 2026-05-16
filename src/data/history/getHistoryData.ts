3f5b7b303d341649a84e4f1af4e7f6c37d7dce70
PYXHD <161946414+PYXHD@users.noreply.github.com>
2026-05-16 21:42:13 +0200

    adjust getHistoryData function

--- src/data/history/getHistoryData.ts
+++ src/data/history/getHistoryData.ts
@@ -4,6 +4,7 @@ import { getAppMode } from "@/lib/init/getAppMode";
 import { demoHistoryViews } from "../sources/mock/demoHistoryViews";
 import { sortHistoryData } from "./sortHistoryData";
 import { supabase } from "@/lib/supabase";
+import { fetchMockHistoryViews } from "./fetchHistoryViews";
 
 type HistoryViewsDB = {
   user_id: string;
@@ -14,13 +15,7 @@ export async function getHistoryData(userId: string) {
   const mode = await getAppMode();
 
   if (mode === "mock") {
-    const result = await supabase
-      .from("mock_history_views")
-      .select("*")
-      .eq("user_id", userId)
-      .order("date", { ascending: true });
-
-    const existing: HistoryViewsDB[] = result.data ?? [];
+    const existing = await fetchMockHistoryViews(userId);
 
     const history: ISODate[] = existing.map((item) => item.date);
 
---
+++ src/data/history/types/HistoryViewsDB.ts
@@ -0,0 +1,6 @@
+import { ISODate } from "./Time";
+
+export type HistoryViewsDB = {
+  user_id: string;
+  date: ISODate;
+};
