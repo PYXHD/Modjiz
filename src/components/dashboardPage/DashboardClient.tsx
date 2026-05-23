b1695e2714cca4d645c113e7a9bc02194402d553
PYXHD <161946414+PYXHD@users.noreply.github.com>
2026-05-23 22:17:33 +0200

    adjust dashboardClient component

--- src/components/dashboardPage/DashboardClient.tsx
+++ src/components/dashboardPage/DashboardClient.tsx
@@ -5,11 +5,12 @@ import { useAppData } from "@/lib/context/AppDataContext";
 import TodayMood from "@/components/dashboardPage/TodayMood";
 import RecentMoods from "@/components/dashboardPage/RecentMoods";
 import { signInAnonymously } from "@/data/id/signInAnonymously";
-import { useEffect } from "react";
-import { supabase } from "@/lib/supabase";
+import { useEffect, useMemo } from "react";
+import { createClient } from "@/lib/supabase/browser";
 
 function DashboardClient() {
   const { userData, currentDate } = useAppData();
+  const supabase = useMemo(() => createClient(), []);
 
   useEffect(() => {
     async function run() {
