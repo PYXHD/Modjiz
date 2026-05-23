65238a9d92fbc3eebd8ec382675662da8cc962ff
PYXHD <161946414+PYXHD@users.noreply.github.com>
2026-05-23 21:53:32 +0200

    adjust middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,9 @@
-// middleware.ts
+import { type NextRequest } from "next/server";
 
-import { NextRequest, NextResponse } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
 
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-
-  const mode = request.cookies.get("app-mode")?.value ?? "mock";
-
-  if (mode === "mock") {
-    const hasMockUserId = request.cookies.has("mock-user-id");
-
-    if (!hasMockUserId) {
-      response.cookies.set("mock-user-id", crypto.randomUUID(), {
-        path: "/",
-        httpOnly: true,
-        maxAge: 60 * 60 * 24 * 365,
-      });
-    }
-  }
-
-  if (mode === "real") {
-    response.cookies.delete("mock-user-id");
-  }
-
-  return response;
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
 }
 
 export const config = {
