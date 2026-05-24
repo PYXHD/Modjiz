33fb05380037fd59ff86d349a59e7018f8082e07
PYXHD <161946414+PYXHD@users.noreply.github.com>
2026-05-24 21:46:09 +0200

    add AuthProvider

--- src/components/dashboardPage/DashboardClient.tsx
+++ src/components/dashboardPage/DashboardClient.tsx
@@ -4,35 +4,9 @@ import { useAppData } from "@/lib/context/AppDataContext";
 
 import TodayMood from "@/components/dashboardPage/TodayMood";
 import RecentMoods from "@/components/dashboardPage/RecentMoods";
-import { signInAnonymously } from "@/data/id/signInAnonymously";
-import { useEffect, useMemo } from "react";
-import { createClient } from "@/lib/supabase/browser";
 
 function DashboardClient() {
   const { userData, currentDate } = useAppData();
-  const supabase = useMemo(() => createClient(), []);
-
-  useEffect(() => {
-    async function run() {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-
-      if (!user) {
-        await signInAnonymously();
-
-        const {
-          data: { user: newUser },
-        } = await supabase.auth.getUser();
-
-        console.log(newUser);
-      } else {
-        console.log(user);
-      }
-    }
-
-    run();
-  }, []);
 
   return (
     <div className="dashboard">
---
+++ src/lib/context/AuthProvider.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+
+import { createClient } from "../supabase/browser";
+
+import { signInAnonymously } from "@/data/id/signInAnonymously";
+
+type AuthContextType = {
+  userId: string | null;
+  loading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const supabase = useMemo(() => createClient(), []);
+
+  const [userId, setUserId] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function run() {
+      const appMode =
+        document.cookie
+          .split("; ")
+          .find((row) => row.startsWith("app-mode="))
+          ?.split("=")[1] ?? "mock";
+
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (appMode === "mock") {
+        if (!user) {
+          await signInAnonymously();
+        }
+      }
+
+      if (appMode === "real") {
+        // Real auth
+      }
+
+      const {
+        data: { user: finalUser },
+      } = await supabase.auth.getUser();
+
+      if (finalUser) {
+        setUserId(finalUser.id);
+      }
+
+      setLoading(false);
+    }
+
+    run();
+  }, [supabase]);
+
+  if (loading) {
+    return <div>Chargement des données...</div>;
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        userId,
+        loading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used inside AuthProvider");
+  }
+
+  return context;
+}
