716312e5a7687baa2878e63f71c7a07df410ac7b
PYXHD <161946414+PYXHD@users.noreply.github.com>
2026-05-27 21:37:47 +0200

    synchronize history views instantly after save

--- src/lib/context/AppDataContext.tsx
+++ src/lib/context/AppDataContext.tsx
@@ -4,24 +4,40 @@ import type { Entry } from "@/types/Entry";
 import type { ISODate } from "@/types/Time";
 import type { ReactNode } from "react";
 
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 type AppData = {
   userData: Entry[];
   historyData: ISODate[];
   currentDate: Date;
+  setHistoryData: React.Dispatch<React.SetStateAction<ISODate[]>>;
 };
 
 type DataProviderProps = {
   children: ReactNode;
-  value: AppData;
+
+  value: {
+    userData: Entry[];
+    historyData: ISODate[];
+    currentDate: Date;
+  };
 };
 
 const AppDataContext = createContext<AppData | null>(null);
 
 export function DataProvider({ children, value }: DataProviderProps) {
+  const [historyData, setHistoryData] = useState(value.historyData);
+
   return (
-    <AppDataContext.Provider value={value}>{children}</AppDataContext.Provider>
+    <AppDataContext.Provider
+      value={{
+        ...value,
+        historyData,
+        setHistoryData,
+      }}
+    >
+      {children}
+    </AppDataContext.Provider>
   );
 }
 
